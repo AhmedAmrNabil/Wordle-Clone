db3f203983dd50bfc5b2f1aff06c547da49782c5
AhmedAmrNabil <ahmedamr24680@gmail.com>
2024-07-21 00:39:03 +0300

    initial wordle commit

---
+++ script.js
@@ -0,0 +1,169 @@
+let wordGrid = document.querySelectorAll(".letter-box");
+let wordRows = document.querySelectorAll(".word-row");
+let toastList = document.querySelector("#messege-list");
+let date = Date.now();
+let todayWord =
+  WORDS[
+    12546 + (Math.floor(date / (60 * 60 * 24 * 1000)) % (WORDS.length - 12546))
+  ];
+let position = 0;
+let row = 0;
+let word = [];
+let gameRunning = true;
+let changingLetters = {};
+let animationRunning = false;
+let win = false;
+let correctGuesses = 0;
+let winnerWords = [
+  "Genius",
+  "Magnificent",
+  "Impressive",
+  "Splendid",
+  "Great",
+  "Phew",
+];
+
+window.addEventListener("keydown", (e) => {
+  if (!gameRunning) return;
+  if (animationRunning) return;
+  e.preventDefault();
+  if (e.code === "Backspace") {
+    deleteLetter();
+    return;
+  } else if (e.key === "Enter") {
+    checkWord();
+    return;
+  } else if (e.code !== `Key${e.key.toUpperCase()}` || position == 5) return;
+  animatePop(wordGrid[5 * row + position]);
+  wordGrid[5 * row + position].innerText = e.key.toLowerCase();
+  word.push(e.key.toLowerCase());
+  position++;
+  console.log(word.join(""));
+});
+
+for (let i = 0; i < 6; ++i) {
+  wordGrid[i * 5 + 4].addEventListener("animationend", () => {
+    animationRunning = false;
+    for (const [key, value] of Object.entries(changingLetters)) {
+      let element = document.querySelector(`button[data-key="${key}"]`);
+      element.classList.add(value);
+    }
+    if (!gameRunning) return;
+    if (row == 6) {
+      gameRunning = false;
+      if (win == false) {
+        let toast = document.createElement("li");
+        toast.innerText = todayWord;
+        toast.classList.add("alert");
+        toastList.insertBefore(toast, toastList.children[0]);
+      } else {
+        winAnimation();
+        let toast = document.createElement("li");
+        toast.innerText = winnerWords[row - 1];
+        toast.classList.add("alert");
+        toastList.insertBefore(toast, toastList.children[0]);
+      }
+    } else if (win == true) {
+      winAnimation();
+      gameRunning = false;
+      let toast = document.createElement("li");
+      toast.innerText = winnerWords[row - 1];
+      toast.classList.add("alert");
+      toastList.insertBefore(toast, toastList.children[0]);
+    }
+  });
+}
+
+function checkWord() {
+  let currWord = todayWord;
+  if (word.length != 5) {
+    shake();
+    let toast = document.createElement("li");
+    toast.innerText = "Not enough letters";
+    toast.classList.add("alert-animation");
+    toastList.insertBefore(toast, toastList.children[0]);
+    toast.addEventListener("animationend", () => {
+      toast.remove();
+    });
+  } else if (!WORDS.includes(word.join(""))) {
+    shake();
+    let toast = document.createElement("li");
+    toast.innerText = "Not in word list";
+    toast.classList.add("alert-animation");
+    toastList.insertBefore(toast, toastList.children[0]);
+    toast.addEventListener("animationend", () => {
+      toast.remove();
+    });
+  } else {
+    animationRunning = true;
+    for (let i = 0; i < word.length; ++i) {
+      wordGrid[5 * row + i].style.animationDelay = `${250 * i}ms`;
+      if (word[i] == currWord[i]) {
+        changingLetters[word[i]] = "correct-letter";
+        wordGrid[5 * row + i].classList.add("correct-guess");
+        currWord = currWord.substring(0, i) + "_" + currWord.substring(i + 1);
+        correctGuesses++;
+      } else if (currWord.includes(word[i])) {
+        changingLetters[word[i]] = "present-letter";
+        wordGrid[5 * row + i].classList.add("present-guess");
+        currWord = currWord.substring(0, i) + "_" + currWord.substring(i + 1);
+      } else {
+        changingLetters[word[i]] = "wrong-letter";
+        wordGrid[5 * row + i].classList.add("wrong-guess");
+      }
+    }
+    if (correctGuesses == 5) {
+      win = true;
+    }
+    correctGuesses = 0;
+    row++;
+    position = 0;
+    word = [];
+  }
+}
+
+function animatePop(element) {
+  element.animate(
+    [
+      { transform: "scale(0.8)", opacity: 0 },
+      { transform: "scale(1.1)", opacity: 1, offset: 0.4 },
+      { transform: "scale(1)", opacity: 1 },
+    ],
+    {
+      duration: 100,
+      iterations: 1,
+    }
+  );
+}
+
+function shake() {
+  if (row >= 6) return;
+  wordRows[row].classList.add("shake");
+  wordRows[row].addEventListener("animationend", () => {
+    wordRows[row].classList.remove("shake");
+  });
+}
+
+function pressLetter(event) {
+  if (position == 5) return;
+  animatePop(wordGrid[5 * row + position]);
+  wordGrid[5 * row + position].innerText = event.innerText.toLowerCase();
+  word.push(event.innerText.toLowerCase());
+  position++;
+}
+
+function deleteLetter() {
+  if (position > 0) {
+    position--;
+    wordGrid[5 * row + position].innerText = "";
+    word.pop();
+    console.log(word.join(""));
+  }
+}
+
+function winAnimation() {
+  for (let i = 0; i < 5; ++i) {
+    wordGrid[5 * (row - 1) + i].style.animationDelay = `${100 * i}ms`;
+    wordGrid[5 * (row - 1) + i].classList.add("hard-shake");
+  }
+}
